00a7b995f22e4fc406cb45c9334d5bce87ccacd2
ROBERTO.ZX <126821263+BLACKWHITY444@users.noreply.github.com>
2026-05-06 15:17:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+    
+  };
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2030-05-21T00:00:00");
+
+
+contadores[0].textContent = cauculatempo(tempoObjetivo1)
+
+
+function cauculatempo(tempoObjetivo){
+  let tempoatual = new Date;
+  let tempofinal = tempoObjetivo - tempoatual;
+  let segundos = Math.floor(tempofinal/1000);
+  let minutos = Math.floor(segundos/60);
+  let horas = Math.floor(minutos/60);
+  let dias = Math.floor(horas/24);
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24
+  return(
+    dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos. "
+  )
+}
